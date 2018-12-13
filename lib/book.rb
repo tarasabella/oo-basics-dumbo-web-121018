ae8d2530f73fcd5eb557bef90ac09d1d8b56860f
Tara Sabella <tarasabella@gmail.com>
2018-12-13 00:52:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,74 @@
+# Failures:
+ 
+#   1) Book ::new gets initialized with a title
+#     Failure/Error: expect{Book.new("And Then There Were None")}.to_not raise_error
+#       expected no Exception, got #<NameError: uninitialized constant Book> with backtrace:
 
+# this error means that our book class is not defined 
 
+class Book 
+  #define a class called Book
+  def initialize(title) 
+    @title = title 
+    #initialize method means that in order for Book to exist, it needs a title
+  end 
+  #Lets give our classes properties with the title, author, page_count, & genre methods through setters and getters 
+  def title 
+    @title 
+    # define title method which is a getter becuase it gets a property for us 
+  end 
+  def author=(author)
+    @author = author
+    #setter for author 
+    #if the title method gets the title property for us, what will set a property or an instance variable 
+    #setters are methods that allow you to set instance variables 
+    #ex. instance.property = "something"
+  end 
+  def author 
+    @author 
+    #getter method that gets author properties 
+  end 
+  def page_count=(page_count)
+    @page_count= page_count
+    #setter method that sets a property or instance variable 
+  end 
+  def page_count
+    @page_count  
+    #getter is method that gets page_count properties for us 
+end 
+  def genre=(genre)
+    @genre =genre 
+    #setter method that sets the genre instance variable or property 
+  end 
+  def genre 
+    @genre 
+    #define genre method with getter that is a method that gets a property or instance variable 
+  end 
+  #How do we give our books a behavior?? 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  #define turn_page method that puts out a sentence 
+end 
+end 
+#Tell your book to turn the page 
+book = Book.new("The Alchemist")
+book.turn_page
+
+#
+#lets simplify 
+class Book 
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title) 
+    @title = title 
+    #initialize method means that in order for Book to exist, it needs a title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+class Shoe 
+  #define class called Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  #to access a property of our shoe within the Shoe class we need a setter and getter for the property 
+  #the setter sets an instance variable & the getter gets a property for us 
+  #attribute accessors give us both a getter and a setter so 
+  #an attr_accessor for :name creates both name and name= methods for #us so it replaces both 
+  
+  #(attribute readers are a getter for free so that attr_reader for    #:name creates a name method for us)
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def cobble
+    
+     puts "Your shoe is as good as new!"
+    
+  end 
+end 
+shoe=Shoe.condition
+    shoe.condition ="new"
+    shoe.cobble
+
